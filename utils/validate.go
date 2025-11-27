6c24313aacacbfdfde3386c589ee594033c8dc2f
rakkiad <rakkiad.ph@rmuti.ac.th>
2025-11-27 15:09:32 +0700

    test(validate): เพมฟังก์ชันตรวจสอบ inp

---
+++ utils/validate.go
@@ -0,0 +1,19 @@
+package utils
+
+import (
+	"errors"
+	"test-backend/models"
+)
+
+func ValidateTextRequest(req models.TextRequest) error {
+	if req.TotalIncome < 0 {
+		return errors.New("total income cannot be negative")
+	}
+	if req.WHT < 0 {
+		return errors.New("WHT cannot be negative")
+	}
+	if req.WHT > req.TotalIncome {
+		return errors.New("WHT cannot be greater than total income")
+	}
+	return nil
+}
