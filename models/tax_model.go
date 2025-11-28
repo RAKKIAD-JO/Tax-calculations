46194bd38fbc878fd93eb6d7c1d97201569eafa4
rakkiad <rakkiad.ph@rmuti.ac.th>
2025-11-28 14:22:12 +0700

    test(tax_model.go): แก้ไข้ชื่อ type

--- models/tax_model.go
+++ models/tax_model.go
@@ -1,22 +1,22 @@
 package models
 
 type Allowance struct {
-	Allowance string  `json:"allowanceType"`
-	Amount    float64 `json:"amount"`
+	AllowanceType string  `json:"allowanceType"`
+	Amount        float64 `json:"amount"`
 }
 
-type TextRequest struct {
-	TotalIncome float64     `json:"totalIncome"`
+type TaxRequest struct {
+	TotalIncome float64     `json:"totalIncome`
 	WHT         float64     `json:"wht"`
 	Allowances  []Allowance `json:"allowances"`
 }
 
-type TextLevel struct {
+type TaxLevel struct {
 	Level string  `json:"level"`
 	Tax   float64 `json:"tax"`
 }
 
 type TaxResponse struct {
-	Tax       float64     `json:"tax"`
-	TaxLevels []TextLevel `json:"taxLevels,omitempty"`
+	Tax       float64    `json:"tax"`
+	TaxLevels []TaxLevel `json:"taxLevals,omitempty"`
 }
